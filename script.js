5adef0b1cf41f180f92d23a06506fd0b0cafdc04
Alexandre Raczinoski <alexandreraczinoski@gmail.com>
2026-03-19 18:17:57 -0300

    Adição de filmes na lista e consumo da API feitas

--- script.js
+++ script.js
@@ -2,11 +2,57 @@ const searchButton = document.getElementById("search-button");
 const overlay = document.getElementById("modal-overlay");
 const movieName = document.getElementById("movie-name");
 const movieYear = document.getElementById("movie-year");
+const movieListContainer = document.getElementById("movie-list");
 
-function searchButtonHandler() {
-  overlay.classList.add("open");
-  console.log(movieName.value.split(" ").join("+"));
-  console.log(movieYear.value);
+let movieList = [];
+
+async function searchButtonHandler() {
+  try {
+    let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieNameParameterGenerator()}${movieYearParameterGenerator()}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.Error) {
+      throw new Error("Filme não encontrado");
+    }
+    createModal(data);
+    overlay.classList.add("open");
+  } catch (error) {
+    notie.alert({
+      type: "error",
+      text: error.message,
+      time: 2,
+    });
+  }
+}
+
+function movieNameParameterGenerator() {
+  if (movieName.value === "") {
+    throw new Error("O nome do filme deve ser infomrado");
+  }
+  return movieName.value.split(" ").join("+");
+}
+
+function movieYearParameterGenerator() {
+  if (movieYear.value === "") {
+    return " ";
+  }
+  if (movieYear.value.length !== 4 || Number.isNaN(Number(movieYear.value))) {
+    throw new Error("Ano do filme inválido");
+  }
+  return `&y=${movieYear.value}`;
+}
+
+function addToList(movieObject) {
+  movieList.push(movieObject);
+}
+
+function updateUI(movieObject) {
+  movieListContainer.innerHTML += `<article>
+    <img src='${movieObject.Poster}' alt='Poster de ${movieObject.Title}' />
+    <button class='remove-button'>
+      <i class='bi bi-trash'></i> Remover
+    </button>
+  </article>`;
 }
 
 searchButton.addEventListener("click", searchButtonHandler);
